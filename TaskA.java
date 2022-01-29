79bd52f476ffab9f349afd4dfac61f5e7c766c6b
NasserA <98425768+NasserA12433@users.noreply.github.com>
2022-01-28 19:08:10 -0800

    Add files via upload

---
+++ TaskA.java
@@ -0,0 +1,17 @@
+
+/**
+ * Nasser Alnabulsi
+ * CS1400
+ * Assignment 1
+ * 9/4/2020
+ */
+public class TaskA
+{
+    String superHeroName="Batman";
+    String enemyName="Riddler";
+    int movies= 3;
+    {
+    System.out.println("My name is " + superHeroName + ". My enemy is The "
+    + enemyName + "." + movies + " movies have been made about me.");
+    }
+}
